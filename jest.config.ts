06fce07605e2c396e9312e85fd57ddf3a7f31004
thiagosngouvea <thiagosngouvea@gmail.com>
2025-11-15 15:53:06 -0300

    refactor: :sparkles: Configuração do next e remoção das configurações do vite, setup inicial

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
+
+export default createJestConfig(customJestConfig);
+
