d6da26db71d5efa8b43cacd31bdfdaa10eeef37b
thiagosngouvea <thiagosngouvea@gmail.com>
2025-11-17 17:34:24 -0300

    feat: :sparkles: implementa layout raiz da aplicação

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import type { Metadata } from 'next';
+import { AppProvider } from '@/src/features/apps/context';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Pluga Challenge Front',
+  description: 'Aplicação para listar e buscar ferramentas integradas à Pluga',
+  icons: {
+    icon: 'https://assets.pluga.co/site/images/favicons/favicon-32x32.png',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <AppProvider>{children}</AppProvider>
+      </body>
+    </html>
+  );
+}
+
