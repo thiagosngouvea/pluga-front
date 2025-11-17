2783a61798d3f1c541c7df8e72dee2a973235ef5
thiagosngouvea <thiagosngouvea@gmail.com>
2025-11-17 17:34:08 -0300

    chore: :sparkles: adiciona exportações centralizadas dos componentes

---
+++ src/features/apps/components/index.ts
@@ -0,0 +1,8 @@
+export { AppCard } from './AppCard';
+export { AppGrid } from './AppGrid';
+export { AppGridContainer } from './AppGridContainer';
+export { AppModal } from './AppModal';
+export { AppModalContainer } from './AppModalContainer';
+export { SearchBar } from './SearchBar';
+export { Pagination } from './Pagination';
+
